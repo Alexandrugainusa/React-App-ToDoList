cd354496971e6d2ba349723598ed062b8a8228c8
Alex's Laptop <alexandruu.gainusa@yahoo.com>
2020-05-24 21:28:12 +0300

    Updates

---
+++ precache-manifest.9ac993142c748d267f2353956a5f006d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1afa866135fd798e5a422ca5e29820df",
+    "url": "/React-App-ToDoList/index.html"
+  },
+  {
+    "revision": "a7deb9aed1f391a4ba52",
+    "url": "/React-App-ToDoList/static/css/main.12b37e23.chunk.css"
+  },
+  {
+    "revision": "072303775e582231bbb2",
+    "url": "/React-App-ToDoList/static/js/2.47462808.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App-ToDoList/static/js/2.47462808.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7deb9aed1f391a4ba52",
+    "url": "/React-App-ToDoList/static/js/main.c73223b0.chunk.js"
+  },
+  {
+    "revision": "2f26c37530e38b2a7171",
+    "url": "/React-App-ToDoList/static/js/runtime-main.9f3ba7a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-App-ToDoList/static/media/logo.5d5d9eef.svg"
+  }
+]);
